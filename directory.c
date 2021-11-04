ad17d4c18ba2be58bba1eb51e83550b1f4422bb1
KenLi8504 <kli22@stuy.edu>
2021-11-04 02:00:04 -0400

    Completed the additional assignment

--- directory.c
+++ directory.c
@@ -3,9 +3,35 @@
 #include <sys/types.h>
 #include <strings.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
 
-int main(){
-  DIR * currentDir = opendir("./");
+int main(int argc, char **argv){
+  char directory[1000];
+  if (argc == 2){
+    strcpy(directory, argv[1]);
+    //printf("%s\n",directory);
+  }
+  else if (argc > 2){
+    printf("Too many arguments my guy, put only 1.\n");
+  }
+  else{
+    char temp[10000];
+    printf("Please input a directory:");
+    fgets(temp,10000,stdin);
+    strncpy(directory,temp,strlen(temp)-1);
+    // printf("The directory is %s\n",directory);
+    // printf("This directory is %lu bytes long \n",strlen(directory));
+  }
+
+  DIR * currentDir;
+  if (opendir(directory) == 0){
+    printf("Error %d: %s \n", errno, strerror(errno));
+    return 0;
+  }
+  else{
+    currentDir = opendir(directory);
+  }
   struct stat temp;
   unsigned long counter = 0;
 
